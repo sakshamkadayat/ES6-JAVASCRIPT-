754a27f733182f8bbf0f5ce4e43b026df3ad936b
sakshamkadayat <sakshamkadayat@gmail.com>
2022-03-24 02:15:48 -1200

    day1

---
+++ Arrowfunction.js
@@ -0,0 +1,2 @@
+const x=(x,y)=>x*y;
+document.getElementById(x(4,5))
---
+++ Varibaleusingconstant.js
@@ -0,0 +1,9 @@
+var x= 10;
+{
+
+const x=2;
+document.getElementById(x);// here x=2
+
+}
+
+document.getElementById(x);  //here x=10 
