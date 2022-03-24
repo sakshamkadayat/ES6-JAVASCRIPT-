dcbae5eb774d73a9e023e7f75c09325ceabb5789
sakshamkadayat <sakshamkadayat@gmail.com>
2022-03-24 01:59:55 -1200

    first commit

---
+++ objectshorthand.js
@@ -0,0 +1,17 @@
+//The shorthand property makes the code size smaller and simpler.
+
+
+// Object property shorthand
+let name = 'IT.chandra'
+let age = 20
+let location = 'Nepal'
+
+// User with ES6 shorthand
+// property
+let user = {
+	name,	
+	age,
+	location
+}
+
+console.log(user)
